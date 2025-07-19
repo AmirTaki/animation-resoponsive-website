7039805fdc9587e89d9685c98541cadaef814e0a
amir <amirtaki623@gmail.com>
2025-07-19 23:06:05 +0330

    add lister click at project

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 let menu = document.querySelector("#menu-icon")
 let navList = document.querySelector(".navlist")
 
+menu.addEventListener("click", (e)=> {
+    menu.classList.toggle("bx-x")  
+    navList.classList.toggle("open")  
+})
